486c7df1beefa96cac3910ae1d46d28d1c2803c3
Marcos Coimbra <marcos.coimbra@rommanel.com.br>
2017-07-27 10:46:47 -0300

    Adicionar arquivos de projeto.

---
+++ AluraNutricao/AluraNutricao/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AluraNutricao
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public void AtualizaCalorias(Object sender, EventArgs e)
+        {
+            //Pegar o valor do slider
+            double valor = sldCalorias.Value;
+            lblCalorias.Text = valor.ToString();
+        }
+
+        public void salvaRefeicao(Object sender, EventArgs e)
+        {
+            string descricao = entDescricao.Text;
+            string calorias = sldCalorias.Value.ToString();
+            string msg = "A refeição " + descricao + " de " + calorias + " calorias foi salva com sucesso";
+            DisplayAlert("Cadastro de Calorias", msg, "OK" );
+            Clear();
+        }
+
+        public void Clear()
+        {
+            entDescricao.Text = "";
+            sldCalorias.Value = 0;
+            
+        }
+        
+    }
+}
